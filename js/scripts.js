d9af2bfcd5c539a0a9c98a70f106fa9e7f91adcf
schuylermk <schuyler.m.k@gmail.com>
2016-01-15 17:22:21 -0800

    Finalize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+
   $("h4").click(function() {
     alert("Don't delay! Fill in below, and get away!");
   });
@@ -12,11 +13,33 @@ $(function() {
   });
 
   $("#weather").click(function() {
-    alert("While you're there, do you care- WARM wind in your hair, or  do you dare for the COOLER air??");
+    alert("While you're there, do you care- WARM wind in your hair, or do you dare for COOLER air??");
   });
 
   $('input[type="checkbox"]').on('change', function() {
     $('input[name="' + this.name + '"]').not(this).prop('checked', false);
   });
+
+var = Berlin = $("input[name=rage]:checked").val();
+var = Aruba = $("input[:checked").val();
+var = Hawaii = $("input[]:checked").val();
+var = Cairo = $("input[]:checked").val();
+var = Nunavut = $("input[]:checked").val();
+var = Moscow = $("input[]:checked").val();
+var = Alaska = $("input[]:checked").val();
+var = LasVegas = $("input[]:checked").val();
+
+var userInput = $("input#group1").val();
+
+$("#yourTrip form").submit(function(event) {
+    var userInput = $("input#group1").val();
+
+    event.preventDefault();
+});
+
+// Based on input the user will be shown where they should holiday.
+// $('p').append('New last sentence of the paragraph.');
+
+
 
 });
