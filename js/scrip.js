92eda66c501d1e6171b911aa48f78599c0ec282a
Schuyler Klaassen <schuyler.m.k@gmail.com>
2016-01-23 20:21:04 -0800

    Put photos in carousel

--- js/scrip.js
+++ js/scrip.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
 
+
     var rr = $('input[name=rr]:checked').val();
     var wild = $('input[name=wild]:checked').val();
     var bacon = $('input[name=bacon]:checked').val();
@@ -8,8 +9,8 @@ $(document).ready(function() {
 
 
     if(((rr === "relax") && (wild === "adventure") && (bacon === "beaches") && (temp === "hot")))  {
-      alert("hey");
-      $("#aruba").show();
+      $("#aruba").scrollTop(700).show();
+
 
     }
   event.preventDefault();
