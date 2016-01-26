eb3b13788b83238b3db5e3dc4e2dc7a553940074
schuylermk <schuyler.m.k@gmail.com>
2016-01-25 20:56:57 -0800

    Loads all branching to complete destination selection

---
+++ README.md
@@ -0,0 +1,34 @@
+# Vacation Planner
+
+#### Web application to suggest vacation destinations to users after they answer a series of either/or questions. January 2016_
+
+#### By _**Schuyler Klaassen**_
+
+## Description
+
+Four questions where a user is given two choices as answers. Based on branching in JS, the user is supplied a suggestion of one of seven destinations to visit.
+
+## Setup/Installation Requirements
+
+* _Google Chrome_
+
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+_schuyler.m.k@gmail.com_
+
+## Technologies Used
+
+* _JavaScript_
+* _Bootstrap 3_
+* _jQuery_
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2016 **Schuyler Klaassen**
--- js/scrip.js
+++ js/scrip.js
@@ -9,10 +9,39 @@ $(document).ready(function() {
 
 
     if(((rr === "relax") && (wild === "adventure") && (bacon === "beaches") && (temp === "hot")))  {
-      $("#aruba").scrollTop(700).show();
-
-
+      $("#aruba").show();
+    } else if(((rr === "relax") && (wild === "party") && (bacon === "beaches") && (temp === "hot")))  {
+    $("#aruba").show();
+    } else if(((rr === "rage") && (wild === "adventure") && (bacon === "museums") && (temp === "hot")))  {
+    $("#aruba").show();
+    } else if(((rr === "relax") && (wild === "party") && (bacon === "beaches") && (temp === "cold")))  {
+    $("#alaska").show();
+    } else if(((rr === "rage") && (wild === "adventure") && (bacon === "beaches") && (temp === "cold")))  {
+    $("#alaska").show();
+    } else if(((rr === "rage") && (wild === "party") && (bacon === "beaches") && (temp === "cold")))  {
+    $("#alaska").show();
+    } else if(((rr === "relax") && (wild === "party") && (bacon === "museums") && (temp === "cold")))  {
+    $("#berlin").show();
+    } else if(((rr === "relax") && (wild === "adventure") && (bacon === "museums") && (temp === "cold")))  {
+    $("#berlin").show();
+    } else if(((rr === "rage") && (wild === "party") && (bacon === "museums") && (temp === "cold")))  {
+    $("#berlin").show();
+    } else if(((rr === "relax") && (wild === "adventure") && (bacon === "museums") && (temp === "hot")))  {
+    $("#cairo").show();
+    } else if(((rr === "relax") && (wild === "party") && (bacon === "museums") && (temp === "hot")))  {
+    $("#cairo").show();
+    } else if(((rr === "rage") && (wild === "party") && (bacon === "museums") && (temp === "hot")))  {
+    $("#cairo").show();
+    } else if(((rr === "rage") && (wild === "adventure") && (bacon === "beaches") && (temp === "hot")))  {
+    $("#hawaii").show();
+    } else if(((rr === "rage") && (wild === "party") && (bacon === "beaches") && (temp === "hot")))  {
+    $("#lasvegas").show();
+    } else if(((rr === "relax") && (wild === "adventure") && (bacon === "beaches") && (temp === "cold")))  {
+    $("#nunavut").show();
+    } else {
+    return false;
     }
+
   event.preventDefault();
   });
 });
